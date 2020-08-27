a73f4bf1c30d42a240c36e63570cbc2a6f32cd88
fengbin <1420485679@qq.com>
2020-08-27 16:48:52 +0800

    分页修改

--- src/com/training/common/utils/SpratePage.java
+++ src/com/training/common/utils/SpratePage.java
@@ -1,22 +1,35 @@
 package com.training.common.utils;
 
-import com.github.pagehelper.Page;
 import com.training.model.PageInfo;
-import com.training.model.Source;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
+
 /*
-* 分页工具类
-*/
+ * 分页工具类
+ */
 @Component
 public class SpratePage<T> {
     public PageInfo<T> sparate(Integer currentPage, Integer pageSize, List<T> list){
         PageInfo<T> pageInfo = new PageInfo<>(list.size(), currentPage, pageSize, 5);
-        if(currentPage==pageInfo.getTotalPage())
-            pageInfo.setList(list.subList((currentPage-1)*pageSize, list.size()));
-        else
-            pageInfo.setList(list.subList((currentPage-1)*pageSize, (currentPage-1)*pageSize+pageSize));
-        return pageInfo;
+        if (list.size()!=0 &&(list.size()>(pageSize+1)))
+        {
+            if(currentPage==pageInfo.getTotalPage())
+                pageInfo.setList(list.subList((currentPage-1)*pageSize, list.size()));
+            else
+                pageInfo.setList(list.subList((currentPage-1)*pageSize, (currentPage-1)*pageSize+pageSize));
+            return pageInfo;
+        }
+        else {
+            PageInfo<T> pageInfo1= new PageInfo<T>(0,1,pageSize,5);
+            pageInfo1.setList(list);
+            return pageInfo1;
+        }
+//        if(currentPage==pageInfo.getTotalPage())
+//            pageInfo.setList(list.subList((currentPage-1)*pageSize, list.size()));
+//        else
+//            pageInfo.setList(list.subList((currentPage-1)*pageSize, (currentPage-1)*pageSize+pageSize));
+//        return pageInfo;
     }
 }
